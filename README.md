2bd91ebedfae6cf73883c416a1415fc68ce4a518
maxjkfc <maxjkfc@gmail.com>
2018-07-16 19:05:29 +0800

    [develop] Modify shellscript
    1. add the 'exec' commandd
    2. add the 'cmd' command
    3. add the useage command

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # dockertools
+
+
+## Descirption
+
+## Image Version
+
+|Image|version|source|
+|---|---|---|
+|cockroach|v2.0.3 | [ cockroachdb/cockroach ](https://hub.docker.com/r/cockroachdb/cockroach/)|
+|mongo | 3.6 | [ mongo ](https://hub.docker.com/_/mongo/)|
+|mongo-express|0.45| [mongo-express](https://hub.docker.com/r/library/mongo-express/tags/) |
+|mongo-admin|latest| [mrvautin/adminmongo](https://hub.docker.com/r/mrvautin/adminmongo/)|
+|redis| 5.0-rc-alpine |[redis](https://hub.docker.com/_/redis/)|
+|redis-cmd| latest| [tenstartups/redis-commander](https://hub.docker.com/r/tenstartups/redis-commander/)|
+
+
+
+
+## tools.sh
+
+```bash
+    tools.sh [cmd]
+
+    Available Commands:
+
+        tools.sh cmd   up
+        tools.sh cmd   down
+        tools.sh exec  [ service  ]
+
+        [service]
+            mongo
+            cockroach
+            redis
+```
---
+++ tools.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+network_name="axolotl"
+
+prefix_name=$(dirname $PWD)
+
+useage()
+{
+    base=$(basename $0)
+    cat <<EOUSAGE
+    $base [cmd]
+
+    Available Commands:
+
+    $base cmd   up
+    $base cmd   down
+
+    $base exec   [ service ]
+
+EOUSAGE
+    exit 1
+
+}
+
+checknetwork()
+{
+    #check network
+    r=$(docker network ls | grep "$network_name")
+    if [  -z "$r" ]; then
+        echo "Not Find Network"
+        docker network create "$network_name"
+    fi
+}
+
+base(){
+
+    # start docker-compose
+    docker-compose  up -d
+
+    # check the docker-compose
+    docker-compose  ps
+
+}
+
+
+init_roach()
+{
+    cmd="docker exec -it dockertools_roachdb_1 ./cockroach"
+    user='admin'
+    db='iosh'
+    # create admin -- no use password
+    $cmd user set "$user" --insecure
+    # create the project db
+    $cmd sql --insecure -e "CREATE DATABASE $db"
+    # Grant the privileges
+    $cmd sql --insecure -e "GRANT ALL ON DATABASE $db TO $user"
+}
+
+exec_container()
+{
+    container_name=$(docker-compose ps | grep $1_ | awk '{print $1}')
+    docker exec -it $container_name sh
+}
+
+
+cmd_container()
+{
+
+    case $1 in 
+        "start")
+            checknetwork
+            base
+            ;;
+        "down")
+            docker-compose down
+            ;;
+        *)
+            echo "Error Cmd"
+            exit 1
+            ;;
+    esac
+}
+
+
+
+
+case $1  in
+    "cmd")
+        cmd_container $2
+        ;;
+    "exec")
+        exec_container $2
+        ;;
+    *)
+        useage
+        echo "Filed Cmd ;"
+        exit 1
+        ;;
+esac
+
+
+
+
